df4e9364dec680e36c2b22bb8aef60ed5d6a5a5c
steveve14 <galbi41031@gmai.com>
2025-03-03 15:13:07 +0900

    만보기 이름 변경 steper -> pedometer

--- app/src/main/java/com/example/myapplication12345/MainActivity.kt
+++ app/src/main/java/com/example/myapplication12345/MainActivity.kt
@@ -49,7 +49,7 @@ class MainActivity : AppCompatActivity() {
         bottomNav.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.navigation_home -> viewPager.currentItem = 0
-                R.id.navigation_stepper -> viewPager.currentItem = 1
+                R.id.navigation_pedometer -> viewPager.currentItem = 1
                 R.id.navigation_camera -> viewPager.currentItem = 2
                 R.id.navigation_ranking -> viewPager.currentItem = 3
                 R.id.navigation_calendar -> viewPager.currentItem = 4
